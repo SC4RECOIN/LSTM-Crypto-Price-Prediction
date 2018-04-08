dd3a6ca0b3f227e0f6d98cc5fb97c6aea980a5ca
Kurtis Streutker <kurtisstreutker@gmail.com>
2018-04-08 11:28:40 -0400

    initial commit

---
+++ historical_data/get_data.py
@@ -0,0 +1,21 @@
+from binance.client import Client
+import numpy as np
+
+
+# load key and secret and connect to API
+keys = open('../keys.txt').readline()
+api = Client(keys[0], keys[1])
+
+# fetch 15 minute candles of all data
+hist = api.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "17 Aug, 2017")
+
+# create numpy object with closing prices
+hist = np.array(hist)
+hist = hist[:, 4]
+
+# data information
+print("\nDatapoints:  {0}".format(hist.shape[0]))
+print("Memory:      {0:.2f} Mb\n".format(hist.nbytes / 1000000))
+
+# save to file as numpy object
+np.save("hist_data", hist)
